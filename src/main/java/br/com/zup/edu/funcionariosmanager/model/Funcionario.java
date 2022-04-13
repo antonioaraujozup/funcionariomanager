1e2acbe39d91f2fb888ee16ab7400cc6b6c3871a
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-13 19:43:14 -0300

    Adicionando método para atualizar o salário

--- src/main/java/br/com/zup/edu/funcionariosmanager/model/Funcionario.java
+++ src/main/java/br/com/zup/edu/funcionariosmanager/model/Funcionario.java
@@ -1,8 +1,12 @@
 package br.com.zup.edu.funcionariosmanager.model;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 public class Funcionario {
@@ -42,5 +46,28 @@ public class Funcionario {
 
     public Long getId() {
         return id;
+    }
+
+    public void atualizaSalario() {
+
+        if (!this.salarioPodeSerAtualizado()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        BigDecimal aumento = this.salario.multiply(new BigDecimal("0.1"));
+
+        this.salario = this.salario.add(aumento);
+    }
+
+    private boolean salarioPodeSerAtualizado() {
+        return tempoDeCasa() >= 1;
+    }
+
+    private int tempoDeCasa() {
+        LocalDate hoje = LocalDate.now();
+
+        int tempoDeCasa = Period.between(this.dataAdmissao, hoje).getYears();
+
+        return tempoDeCasa;
     }
 }
